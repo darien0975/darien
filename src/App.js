1a2cb5559ed9487bfafa93afb1db1e5aa94961fa
Darien Chen <darien7557@fakemail.com>
2023-05-17 21:11:05 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Nav from "./Nav"
+import Info from "./Info"
+console.log(Nav);
+
+function App() {
+  return 
+   <div>
+      
+    </div>
+    
+  
+}
+
+export default App;
---
+++ src/Info.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div><h1>這是Info組件</h1></div>
+  )
+}
+
+export default Info;
